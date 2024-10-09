f030c4183e613ce9dc1b3c51b91f21e818324a11
Mohamed <mohamed_mazin10@hotmail.com>
2024-10-09 15:00:30 +0300

    Initial Commit

---
+++ CICD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICD
+//
+//  Created by Mohamed Mazen on 09/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
